9c2402d171feb3e2b1f70c29d5dee43385ca4484
SirFluffyEagle <54653943+SirFluffyEagle@users.noreply.github.com>
2019-09-21 14:21:18 -0400

    Revert "init push"
    This reverts commit 1ae717bf44d7d6a9973ee7c7d9116060c8c8e8e8.

---
+++ TutoralJam/Assets/Scripts/AnimationScript.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimationScript : MonoBehaviour
+{
+    public Sprite[] animationFrames = new Sprite[4]; //replace 4 with number of frames
+                                                     //after compiling, you will be able to add sprites from the inspector
+
+    public static float animationTimerMax = 0.2f; //The time in seconds a frame lasts
+    float animationTimer = animationTimerMax;
+
+    public int animationFrame = 0;
+    public static int animationFrameMax;
+
+    SpriteRenderer sr;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+         animationFrameMax = animationFrames.Length - 1;
+
+         sr = GetComponent<SpriteRenderer>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        animationTimer -= Time.deltaTime;
+        if (animationTimer <= 0)
+        {
+            animationTimer += animationTimerMax;
+            animationFrame++;
+            if (animationFrame > animationFrameMax)
+            {
+                animationFrame = 0;
+            }
+
+            sr.sprite = animationFrames[animationFrame];
+        }
+    }
+}
