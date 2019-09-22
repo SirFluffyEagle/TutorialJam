040902008daf32bfce6be0d53707dde6b4966256
AlphaWolf464 <graham@coddsquad.com>
2019-09-22 16:41:02 -0400

    Final

--- TutoralJam/Assets/Scripts/PlayerController.cs
+++ TutoralJam/Assets/Scripts/PlayerController.cs
@@ -3,6 +3,8 @@
 [RequireComponent(typeof(BoxCollider2D))]
 public class CharacterController2D : MonoBehaviour
 {
+    Animator animator;
+
     [SerializeField, Tooltip("Max speed, in units per second, that the character moves.")]
     float speed = 9;
 
@@ -18,34 +20,57 @@ public class CharacterController2D : MonoBehaviour
     private bool grounded;
 
     [SerializeField, Tooltip("Max height the character will jump regardless of gravity")]
-    float jumpHeight = 4;
+    float jumpForce = 4;
 
     private BoxCollider2D boxCollider;
+    private Rigidbody2D rb2D;
 
     private Vector2 velocity;
+    public float timer;
 
     private void Awake()
     {
+        timer = -1;
         boxCollider = GetComponent<BoxCollider2D>();
+        rb2D = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
+        animator.SetBool("Running", true);
+        animator.SetBool("Jump_Key_Pressed", false);
     }
 
     private void Update()
     {
+        if (timer >= 0)
+        {
+            timer = timer + Time.deltaTime;
+        }
         float moveInput = Input.GetAxisRaw("Horizontal");
+
         if (moveInput != 0)
         {
-            velocity.x = Mathf.MoveTowards(velocity.x, speed * moveInput, walkAcceleration * Time.deltaTime);
+
+            velocity.x = Mathf.Lerp(rb2D.velocity.x, speed * moveInput, walkAcceleration * Time.deltaTime);
+            animator.SetBool("Running", true);
         }
         else
         {
-            velocity.x = Mathf.MoveTowards(velocity.x, 0, groundDeceleration * Time.deltaTime);
+            velocity.x = Mathf.Lerp(rb2D.velocity.x, 0, groundDeceleration * Time.deltaTime);
+            animator.SetBool("Running", false);
         }
-        transform.Translate(velocity * Time.deltaTime);
+
+        rb2D.AddForce(velocity - rb2D.velocity);
 
         if (Input.GetKeyDown(KeyCode.W))
         {
-            velocity.y = Mathf.Sqrt(2 * jumpHeight * Mathf.Abs(Physics2D.gravity.y));
+            rb2D.AddForce(new Vector2(0f, jumpForce), ForceMode2D.Impulse);
+            animator.SetBool("Jump_Key_Pressed", true);
+            timer = 0.0f;
+        }     
+        
+        if (timer >= 1)
+        {
+            animator.SetBool("Jump_Key_Pressed", false);
+            timer = -1;
         }
-        velocity.y += Physics2D.gravity.y * Time.deltaTime;
     }
 }
